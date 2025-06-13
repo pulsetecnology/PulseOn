161685c02ac5335ece20a8ba047382cb6c60f7db
André Luis da Silva <43666903-andreluiss1@users.noreply.replit.com>
2025-06-13 05:12:44 +0000

    Restored to '4bc4b37bb31a6b28f91c9a74c58e58474bc27e63'
    Replit-Restored-To: 4bc4b37bb31a6b28f91c9a74c58e58474bc27e63

--- client/src/components/Layout.tsx
+++ client/src/components/Layout.tsx
@@ -9,7 +9,7 @@ export default function Layout({ children }: LayoutProps) {
   return (
     <div className="min-h-screen flex flex-col">
       <Header />
-      <main className="flex-1 pt-16 pb-20 px-6">
+      <main className="flex-1 pt-16 pb-20">
         {children}
       </main>
       <BottomNavigation />
